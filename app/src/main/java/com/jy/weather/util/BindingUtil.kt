43de7647df2b6274e539c925d4e047f14a83654f
liyang <liyang4@yy.com>
2019-03-22 20:11:19 +0800

    last file to kotlin

---
+++ app/src/main/java/com/jy/weather/util/BindingUtil.kt
@@ -0,0 +1,19 @@
+package com.jy.weather.util
+
+import android.databinding.BindingAdapter
+import android.view.ViewGroup
+import android.widget.ImageView
+
+/**
+ * Created by liyang
+ * on 2018/12/28
+ */
+@BindingAdapter("imageId")
+fun setImage(view: ImageView, resId: Int) {
+    view.setImageResource(resId)
+}
+
+@BindingAdapter("backgroundId")
+fun setBackground(viewGroup: ViewGroup, bgId: Int) {
+    viewGroup.setBackgroundResource(bgId)
+}
