ce100ddd6a3c23983740cede81b30eae32ca7b3b
liyang4 <liyang4@yy.com>
2021-03-29 17:16:11 +0800

    remove unless implementation

--- app/src/main/java/com/jy/weather/JYApplication.kt
+++ app/src/main/java/com/jy/weather/JYApplication.kt
@@ -3,10 +3,7 @@ package com.jy.weather
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
-
 import com.jy.weather.data.local.CityDB
-import com.tencent.bugly.Bugly
-import com.tendcloud.tenddata.TCAgent
 import org.litepal.LitePal
 
 /**
@@ -15,7 +12,6 @@ import org.litepal.LitePal
  *
  * Created by Yang on 2018/1/5.
  */
-
 class JYApplication : Application() {
 
     companion object {
@@ -31,22 +27,10 @@ class JYApplication : Application() {
 
         // litepal数据库
         LitePal.initialize(this)
-
-        // Bugly初始化
-        Bugly.init(applicationContext, "dfbeff5d40", true)
-
-        // TalkingData初始化
-        TCAgent.LOG_ON = true
-        // App ID: 在TalkingData创建应用后，进入数据报表页中，在“系统设置”-“编辑应用”页面里查看App ID。
-        // 渠道 ID: 是渠道标识符，可通过不同渠道单独追踪数据。
-        TCAgent.init(this, "09AAE34B970B4C96BBE79940F841311E", "jyweather")
-        // 如果已经在AndroidManifest.xml配置了App ID和渠道ID，调用TCAgent.init(this)即可；或与AndroidManifest.xml中的对应参数保持一致。
-        TCAgent.setReportUncaughtExceptions(true)
     }
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
-
         MultiDex.install(base)
     }
 }
